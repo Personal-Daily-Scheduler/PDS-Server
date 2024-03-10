856b053e1dd17c81af4c6ab6b314cff1864f3109
kyeongjun-ko <gyeongjungo@Gyeongjunui-MacBookAir.local>
2024-03-10 18:31:16 +0900

    📦️ [chore] eslint + prettier + airbnb 설정

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": ["airbnb-base", "prettier", "plugin:node/recommended"],
+    "overrides": [
+        {
+          "env": {
+              "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": ["prettier"],
+    "rules": {
+        "prettier/prettier": "error",
+        "no-unused-vars": "warn",       
+        "no-param-reassign": 0,
+        "func-names": "off",
+    }
+}
