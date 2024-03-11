49b91014d852ab05df3763f0875ef196d8ae9f71
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-11 19:21:49 +0900

    🎨 [style] user 스키마구조 타입 추가

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,9 +3,15 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
 });
 
 const Users = mongoose.model("User", userSchema);
