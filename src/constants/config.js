f09d1e0a4ada3b1acff24a2b5ca1a96f9f6f557a
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-11 19:23:20 +0900

    📦️ [chore] JWT 토큰 발행 문자열, Bcrypt factor 수 환경변수 설정

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const CONFIG = {
   MONGODB_ENDPOINT: process.env.MONGODB_ENDPOINT,
+  BCRYPT_FACTOR: Number(process.env.BCRYPT_COST_FACTOR),
+  SECRET_KEY: process.env.JWT_SECRET_KEY,
 };
 
 module.exports = CONFIG;
