dd07d45fb3f20f4c6c66ff5391acf049ba637819
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-11 16:42:43 +0900

    🎨 [style] 회원가입 API 응답 형태 통일

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,17 +7,19 @@ const registerUser = async (req, res) => {
     const existingCheck = await User.findOne({ email });
 
     if (existingCheck) {
-      return res.status(409).json({ error: "이미 사용 중인 아이디입니다." });
+      return res
+        .status(409)
+        .json({ result: false, message: "이미 사용 중인 아이디입니다." });
     }
 
     const newUser = new User({ username, email, password });
     await newUser.save();
 
-    res.status(201).json({ message: "회원가입에 성공했습니다." });
+    res.status(201).json({ result: true, message: "회원가입에 성공했습니다." });
   } catch (error) {
     console.error(error);
 
-    res.status(500).json({ error: "내부 서버 오류 발생" });
+    res.status(500).json({ result: false, error: "내부 서버 오류 발생" });
   }
 };
 
