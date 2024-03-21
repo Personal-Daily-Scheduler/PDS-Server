b202c0a1a4d1e59e4f02e81d45deb37f361398cf
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-21 16:57:07 +0900

    ✨ [feature] users 라우터 작성

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,11 +1,29 @@
 const express = require("express");
-const userController = require("../controllers/userController");
+const planController = require("../controllers/planController");
+const scheduleController = require("../controllers/scheduleController");
+const diaryController = require("../controllers/diaryController");
 
 const router = express.Router();
 
-router.get("/:userId/diaries/plans", userController.getUserPlans);
-router.post("/:userId/diaries/:date/plan", userController.saveUserPlan);
-router.delete("/:userId/diaries/:date/plan", userController.deleteUserPlan);
-router.put("/:userId/diaries/:date/plan", userController.updateUserPlan);
+router.get("/:userId/diaries/plans", planController.getUserPlans);
+router.get("/:userId/diaries/schedules", scheduleController.getUserSchedules);
+router.get("/:userId/diaries/diaries", diaryController.getUserDiaries);
+router.post("/:userId/diaries/:date/plan", planController.saveUserPlan);
+router.post(
+  "/:userId/diaries/:date/schedule",
+  scheduleController.saveUserSchedule,
+);
+router.post("/:userId/diaries/:date/diary", diaryController.saveUserDiary);
+router.delete("/:userId/diaries/:date/plan", planController.deleteUserPlan);
+router.delete(
+  "/:userId/diaries/:date/schedule",
+  scheduleController.deleteUserSchedule,
+);
+router.delete("/:userId/diaries/:date/diary", diaryController.deleteUserDiary);
+router.put("/:userId/diaries/:date/plan", planController.updateUserPlan);
+router.put(
+  "/:userId/diaries/:date/schedule",
+  scheduleController.updateUserSchedule,
+);
 
 module.exports = router;
