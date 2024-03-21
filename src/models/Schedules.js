811e0c7f6902731717feea4ea98cf20f192ba618
kyeong-ko <rhrudwnszoq@naver.com>
2024-03-21 16:51:31 +0900

    ✨ [feature] schedule 스키마 구조 설계

---
+++ src/models/Schedules.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const ScheduleSchema = new mongoose.Schema({
+  scheduleId: { type: String, require: true },
+  title: { type: String, require: true },
+  description: { type: String },
+  selectedDate: { type: String },
+  colorCode: { type: String },
+  startTime: { type: String },
+  endTime: { type: String },
+});
+
+const UserScheduleSchema = new mongoose.Schema({
+  userId: { type: String },
+  date: { type: String, required: true },
+  schedules: [ScheduleSchema],
+});
+
+const UserSchedule = mongoose.model("UserSchedule", UserScheduleSchema);
+
+module.exports = UserSchedule;
